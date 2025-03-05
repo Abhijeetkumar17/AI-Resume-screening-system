4ed6830885afde25c9060dca8846b30d0557b11c
Abhijeet kumar <119649462+Abhijeetkumar17@users.noreply.github.com>
2025-03-05 15:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,5 +58,8 @@ def upload():
     return render_template("index.html", result=result)
 
 # Run Flask App
+import os
+
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
